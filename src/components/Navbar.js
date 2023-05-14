96724251b550e2469e95f8ece0d1e131fb7980ce
Tiffany <tapintobeauty08@gmail.com>
2023-05-14 17:40:49 -0400

    reverse code changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import { useState } from 'react';
+// import Card from './components/Card';
 
 // We declare an object called styles that will contain a few objects for card and heading styles
 // Notice that each key lists CSS styles in camel case
@@ -19,14 +20,30 @@ const styles = {
 };
 
 // In Navbar, we can assign a style from an object by using curly braces
-function Navbar(props) {
+function Navbar() {
+  // let [link, setLink] = useState(Card.props.id);
+
+  // const handleClick = () {
+  //   setLink((""));
+  //   console.log(link.target.value);
+  // };
 
   return (
     <div style={styles.card}>
       <div style={styles.heading}>
-        <button type="button" className="btn toggle-btn" aria-pressed={props.login} id={props.id}>
+        <button type="button" className="btn toggle-btn" aria-pressed="true" >
           <span className="visually-hidden">Show </span>
-          <span>{props.name}</span>
+          <span>About Me</span>
+          <span className="visually-hidden"> card</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false" >
+          <span className="visually-hidden">Show </span>
+          <span>My Work</span>
+          <span className="visually-hidden"> card</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false" >
+          <span className="visually-hidden">Show </span>
+          <span>Contact Me</span>
           <span className="visually-hidden"> card</span>
         </button>
       </div>
--- src/pages/NavBtns.js
+++ src/pages/NavBtns.js
@@ -1,41 +1,41 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
+// import React from 'react';
+// import Navbar from '../components/Navbar';
 
 
-function NavBtns() {
-    const navBtnData = [
-        {
-            id: 1,
-            name: "About Me",
-            login: "true"
-        },
-        {
-            id: 2,
-            name: "My Work",
-            login: "false"
-        },
-        {
-            id: 3,
-            name: "Contact Me",
-            login: "false"
-        },
-    ]
+// function NavBtns() {
+//     const navBtnData = [
+//         {
+//             id: 1,
+//             name: "About Me",
+//             login: "true"
+//         },
+//         {
+//             id: 2,
+//             name: "My Work",
+//             login: "false"
+//         },
+//         {
+//             id: 3,
+//             name: "Contact Me",
+//             login: "false"
+//         },
+//     ]
 
-    return (
-        <div className="navBtn-container">
-            {
-                navBtnData.map(navBtn => {
-                    return <Navbar
-                        id={navBtn.id}
-                        name={navBtn.name}
-                        login={navBtn.login}
+//     return (
+//         <div className="navBtn-container">
+//             {
+//                 navBtnData.map(navBtn => {
+//                     return <Navbar
+//                         id={navBtn.id}
+//                         name={navBtn.name}
+//                         login={navBtn.login}
 
-                    />
-                })
-            }
+//                     />
+//                 })
+//             }
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default NavBtns;
+// export default NavBtns;
